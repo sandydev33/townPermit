72a3a9acf7c9a4d58af95cab46befb18b262a564
Esoft <esoft@Monu.local>
2019-01-28 13:13:32 +0530

    first commit

---
+++ Parking/GolfViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GolfViewController.swift
+//  Parking
+//
+//  Created by khim singh on 23/10/17.
+//  Copyright © 2017 eSoft. All rights reserved.
+//
+
+import UIKit
+
+class GolfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
